2621fe0981c70508cfdc13d09099eb947d65ad5e
Svale09 <svalinaivan09+git@gmail.com>
2023-02-07 21:06:13 +0100

    Implemented a simple splash screen

--- app/src/main/java/com/example/cooke/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/cooke/ui/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.cooke.ui.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,6 +11,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.cooke.data.di.dataModule
 import com.example.cooke.ui.favorites.di.favoritesModule
 import com.example.cooke.ui.home.di.homeModule
@@ -21,6 +24,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         startKoin{
@@ -33,6 +37,12 @@ class MainActivity : ComponentActivity() {
                 recipeDetailsModule,
                 favoritesModule
             )
+        }
+
+        installSplashScreen().apply {
+            setKeepOnScreenCondition{
+                viewModel.isLoading.value
+            }
         }
         setContent {
             CookETheme {
